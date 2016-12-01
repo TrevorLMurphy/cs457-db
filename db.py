7c38f5c830f6413f768178584e7c09f3908ae3b5
Trevor Murphy <murphtrevor@gmail.com>
2016-11-30 18:09:47 -0600

    need to work on nesting conditionals

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ CS 457 Mongo Clone
 """
 
 import sys
+import operator
 
 def parse_file(arg):
     """
@@ -32,19 +33,82 @@ def get_condition_and_field(string):
     while string[i] != ",":
         condition += string[i]
         i += 1
+
+    if string[i + 1] == " ": # Skip a space if needed
+        i += 1
+
     while i < len(string):
         field += string[i]
         i += 1
-    return (condition, field[2:].replace(")", ""))
+    return (condition, field[1:].replace(")", ""))
+
+def process_cond(cond):
+    key = ""
+    val = ""
+    key_vals = []
+    i = 1
+    while i < len(cond):
+        if key == 'and' or key == 'or':
+            key_vals.append(key)
+            key = ""
+            i += 1
+        elif cond[i] == '=' or cond[i] == '<' or cond[i] == '>' or cond[i] == '!':
+            oper = cond[i]
+            if cond[i+1] == '=':
+                oper += cond[i + 1]
+                i += 2
+            else:
+                i += 1
+            while cond[i] != " " and cond[i] != ')':
+                val += cond[i]
+                i += 1
+            key_vals.append((key, oper, val))
+            key = ""
+            val = ""
+            i += 1
+        else:
+            key += cond[i]
+            i += 1
+    return key_vals
+
+def get_operator(op):
+    return {
+        '=' : operator.eq,
+        '!=' : operator.ne,
+        '<' : operator.lt,
+        '<=' : operator.le,
+        '>' : operator.gt,
+        '>=' : operator.ge
+        }[op]
+
+def perform_cond(cond, doc):
+    return cond[0] in doc and get_operator(cond[1])(doc[cond[0]], cond[2])
 
 def get_response(cond, fields, data):
     """
     Query response
     """
-    for doc in data:
-        if 'Age' in doc and doc['Age'] == 20:
-            print doc
-    print cond, fields
+    conditions = process_cond(cond)
+    result = []
+    is_and = False
+    is_or = False
+    for elem in conditions:
+        if elem == 'and':
+            is_and = True
+            is_or = False
+        elif elem == 'or':
+            is_and = False
+            is_or = True
+        else:
+            if is_and:
+                result = [doc for doc in result if perform_cond(elem, doc)]
+            else:
+                for doc in data:
+                    if perform_cond(elem, doc) and doc not in result:
+                        result.append(doc)
+
+    for doc in result:
+        print doc
 
 def process_query(query, data):
     """
